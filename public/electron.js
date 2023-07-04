a1df9cabeef3cef10534d9d897957fa36ab4ec48
kkamara <kamaracomputers@gmail.com>
2023-07-04 14:46:54 +0100

    Close child window

--- public/electron.js
+++ public/electron.js
@@ -1,11 +1,13 @@
 const electron = require('electron');
 const app = electron.app;
+const ipcMain = electron.ipcMain;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 
 const inProduction = app.isPackaged;
 
 let mainWindow;
+let childWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({ 
@@ -15,6 +17,7 @@ function createWindow() {
             devTools: inProduction ? false : true,
             nodeIntegration: true,
             contextIsolation: false,
+            enableRemoteModule: true,
         },
         show: false,
      });
@@ -30,6 +33,40 @@ function createWindow() {
 
     mainWindow.on('closed', () => (mainWindow = null));
 }
+
+// Function to create child window of parent one
+function createChildWindow() {
+    childWindow = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        modal: true,
+        show: false,
+        parent: mainWindow,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
+    
+    childWindow.loadURL(`file://${path.join(
+        __dirname, 
+        '../', 
+        'src/views/settings.html',
+    )}`);
+    
+    childWindow.once("ready-to-show", () => {
+      childWindow.show();
+    });
+}
+
+ipcMain.on("openChildWindow", (event, arg) => {
+    createChildWindow();
+});
+
+ipcMain.on("closeChildWindow", (event, arg) => {
+    childWindow.close();
+});
 
 app.on('ready', createWindow);
 
