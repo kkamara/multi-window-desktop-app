3d8359644204e6e0f57e515e8966e1770ef8cdf2
kkamara <kamaracomputers@gmail.com>
2023-07-04 17:55:24 +0100

    Add documentation

--- public/electron.js
+++ public/electron.js
@@ -25,7 +25,7 @@ function createWindow() {
     mainWindow.loadURL(`file://${path.join(
         __dirname, 
         '../', 
-        'src/views/index.html',
+        'src/views/index.html', // index.pug?exampleArg=test
     )}`);
 
     mainWindow.maximize();
